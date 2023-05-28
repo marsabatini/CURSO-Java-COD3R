3763097680bbe8dbfa4f06c08ec13f5e4a9e2fcd
Marco Sabatini <108834593+marsabatini@users.noreply.github.com>
2023-05-28 12:47:40 -0300

    V. 0.03

---
+++ Seção 05/PrimeiroTrauma.java
@@ -0,0 +1,13 @@
+package Modulo_05;
+
+public class PrimeiroTrauma {
+
+    int a = 15;
+
+    public static void main(String[] args) {
+
+        PrimeiroTrauma pegaAliEmCima = new PrimeiroTrauma();
+        System.out.println(pegaAliEmCima.a);
+
+    }
+}
