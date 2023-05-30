4e5be376c6ffaea7bee48e410bf11b9bccc4c8a0
marsabatini <marco_rsabatini@hotmail.com>
2023-05-30 17:17:27 -0300

    V. 0.05

---
+++ Seção 07/abstrato/Mamifero.java
@@ -0,0 +1,11 @@
+package Modulo_07.oo.abstrato;
+
+public abstract class Mamifero extends Animal {
+
+    public abstract String mamar();
+
+//    @Override
+//    public final String mover() {
+//        return "saindo do lugar.";
+//    }
+}
