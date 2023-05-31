fb8b20392eae639075e944fdb0fb4ebf79b64696
marsabatini <marco_rsabatini@hotmail.com>
2023-05-31 13:29:46 -0300

    V. 0.06

---
+++ Seção 07/Desafio/Cliente.java
@@ -0,0 +1,23 @@
+package Modulo_07.Desafio1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente {
+    final String nome;
+    final List<Compra> compras = new ArrayList<>();
+
+    Cliente(String nome){
+        this.nome = nome;
+    }
+
+    double obterValorTotal(){
+        double total = 0;
+
+        for(Compra compraAtual: compras){
+            total += compraAtual.obterValorTotal();
+        }
+
+        return total;
+    }
+}
---
+++ Seção 07/Desafio/Item.java
@@ -0,0 +1,16 @@
+package Modulo_07.Desafio1;
+
+import java.util.ArrayList;
+
+public class Item {
+
+    final int quantidade;
+
+    final Produto produto;
+
+   Item(Produto produto, int quantidade){
+       this.produto = produto;
+       this.quantidade = quantidade;
+   }
+
+}
