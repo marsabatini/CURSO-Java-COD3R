22b14157a08575b77036ff4714755b003719cd96
marsabatini <marco_rsabatini@hotmail.com>
2023-06-01 12:49:54 -0300

    V. 0.07

---
+++ Seção 08/lambdas/CalculoTeste2.java
@@ -0,0 +1,16 @@
+package Modulo_08.lambdas;
+
+public class CalculoTeste2 {
+
+    public static void main (String[] args){
+
+        Calculo calc = (x, y) -> {
+          return x + y;
+        };
+
+        System.out.println(calc.executar(2,3));
+
+        calc = (x, y) -> x * y;
+        System.out.println(calc.executar(2, 3));
+    }
+}
