0d1ebb541ac6607a105c735d461e6b4c40ed7c8a
marsabatini <marco_rsabatini@hotmail.com>
2023-06-01 12:53:28 -0300

    V. 0.07

---
+++ Seção 08/lambdas 2/PredicadoComposicao.java
@@ -0,0 +1,15 @@
+package Modulo_08.lambdas;
+
+import java.util.function.Predicate;
+
+public class PredicadoComposicao {
+    public static void main(String[] args) {
+
+        Predicate<Integer> isPar = num -> num % 2 == 0;
+        Predicate<Integer> isTresDigitos = num -> num >= 100 && num <= 999;
+
+        System.out.println(isPar.and(isTresDigitos).negate().test(123));
+        System.out.println(isPar.or(isTresDigitos).test(123));
+
+    }
+}
