9ab43f057da137e9da1b9182410b8058f7542c20
marsabatini <marco_rsabatini@hotmail.com>
2023-06-02 18:09:06 -0300

    V. 0.08

---
+++ Seção 09/streams 02/teste.java
@@ -0,0 +1,10 @@
+package Modulo_09.streams;
+
+public class teste {
+    public static void main(String[] args) {
+        String[] nomes = {"Camila", "Venilton", "Leonardo", "Renan", "Rafael"};
+
+        System.out.print(nomes.length);
+
+    }
+}
