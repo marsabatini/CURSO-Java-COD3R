b2f67c244e57b583a70ace27a1b7bfd5a0750177
marsabatini <marco_rsabatini@hotmail.com>
2023-06-03 13:14:04 -0300

    V. 0.09

---
+++ Seção 12/Generics/CaixaInt.java
@@ -0,0 +1,5 @@
+package Modulo_12.Generics;
+
+public class CaixaInt extends Caixa<Integer>{
+
+}
---
+++ Seção 12/Generics/CaixaIntTest.java
@@ -0,0 +1,13 @@
+package Modulo_12.Generics;
+
+public class CaixaIntTest {
+
+    public static void main(String[] args) {
+
+        CaixaInt caixaA = new CaixaInt();
+        caixaA.guardar(123);
+
+        Integer coisaA = caixaA.abrir();
+        System.out.println(coisaA);
+    }
+}
