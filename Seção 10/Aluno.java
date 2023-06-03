19723aa2ea107f3d7229661196013032dfb2aafa
marsabatini <marco_rsabatini@hotmail.com>
2023-06-03 13:08:58 -0300

    V. 0.09

---
+++ Seção 10/Aluno.java
@@ -0,0 +1,38 @@
+package Modulo_10;
+
+import java.util.Objects;
+
+public class Aluno {
+
+    public final String nome;
+
+    public double nota;
+
+    public boolean bomComportamento;
+
+    public Aluno(String nome, double nota){
+        this(nome, nota, true);
+    }
+
+    public Aluno(String nome, double nota, boolean bomComportamento) {
+        this.nome = nome;
+        this.nota = nota;
+        this.bomComportamento = bomComportamento;
+    }
+
+    public String toString(){
+        return nome + " tem nota " + nota;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Aluno aluno)) return false;
+        return Double.compare(aluno.nota, nota) == 0 && bomComportamento == aluno.bomComportamento && Objects.equals(nome, aluno.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, nota, bomComportamento);
+    }
+}
