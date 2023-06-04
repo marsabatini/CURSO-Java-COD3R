e18d16a55eff2c24e1beea4b6972a153097cc469
marsabatini <marco_rsabatini@hotmail.com>
2023-06-04 08:28:13 -0300

    V. 1.01

---
+++ Seção 13/PadroesObserver/EventoChegadaAniversariante.java
@@ -0,0 +1,17 @@
+package Modulo_13.PadroesObserver;
+
+import java.util.Date;
+
+public class EventoChegadaAniversariante {
+
+    private final Date momento;
+
+
+    public EventoChegadaAniversariante(Date momento) {
+        this.momento = momento;
+    }
+
+    public Date getMomento() {
+        return momento;
+    }
+}
