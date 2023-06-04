2d803016e4a50b29c33cbe647fcf4ba3eb66bdc9
marsabatini <marco_rsabatini@hotmail.com>
2023-06-04 08:31:39 -0300

    V. 1.01

---
+++ Seção 13/Swing/Observador.java
@@ -0,0 +1,27 @@
+package Modulo_13.Swing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Observador {
+
+    public static void main (String[] args){
+
+        JFrame janela = new JFrame("Observador");
+        janela.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        janela.setSize(600, 200);
+        janela.setLayout(new FlowLayout());
+        janela.setLocationRelativeTo(null); // Centralizar na Tela do computador
+
+        JButton botao = new JButton("Clicar!");
+        janela.add(botao);
+
+        botao.addActionListener(e ->{
+            System.out.println("Evento ocorreu!!!");
+        });
+
+        janela.setVisible(true);
+    }
+}
