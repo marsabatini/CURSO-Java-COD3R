c77c3470f0bcc39e41d9fbb043a5a36ffbc4a048
marsabatini <marco_rsabatini@hotmail.com>
2023-06-06 13:49:33 -0300

    V. 1.03

---
+++ Seção - Projetos/Calculadora/visão/Display.java
@@ -0,0 +1,30 @@
+package calc.visao;
+
+import calc.modelo.Memoria;
+import calc.modelo.MemoriaObservador;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Display extends JPanel implements MemoriaObservador {
+
+    private final JLabel label;
+
+    public Display(){
+        Memoria.getInstance().adicionarObservador(this);
+
+        setBackground(new Color(46, 49, 50));
+        label = new JLabel(Memoria.getInstance().getTextoAtual());
+        label.setForeground(Color.WHITE);
+        label.setFont(new Font("courier", Font.PLAIN, 30));
+
+        setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+
+        add(label);
+    }
+
+    @Override
+    public void valorAlterado(String novoValor) {
+        label.setText(novoValor);
+    }
+}
