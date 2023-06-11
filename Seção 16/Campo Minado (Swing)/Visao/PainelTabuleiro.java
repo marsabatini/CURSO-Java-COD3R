dd3bb49d4ecec9c413e0e1c2c1051e8d85f7173a
marsabatini <marco_rsabatini@hotmail.com>
2023-06-11 09:34:44 -0300

    V. 1.04

---
+++ Seção 16/Campo Minado (Swing)/Visao/PainelTabuleiro.java
@@ -0,0 +1,29 @@
+package br.com.sabatini.cm.Visao;
+
+import br.com.sabatini.cm.Modelo.Tabuleiro;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PainelTabuleiro extends JPanel {
+
+    public PainelTabuleiro(Tabuleiro tabuleiro){
+
+        setLayout(new GridLayout(tabuleiro.getLinhas(), tabuleiro.getColunas()));
+
+        tabuleiro.paraCadaCampo(c -> add(new BotaoCampo(c)));
+        tabuleiro.registrarObservador(e -> {
+
+            SwingUtilities.invokeLater(() ->{
+                if(e.isGanhou()){
+                    JOptionPane.showMessageDialog(this, "Ganhouuu :)");
+                } else {
+                    JOptionPane.showMessageDialog(this, "Perdeu! :(");
+                }
+
+                tabuleiro.reiniciar();
+            });
+        });
+
+    }
+}
